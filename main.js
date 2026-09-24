683d1c3863c03015f1937e2f2fe946c413606503
laizzy0 <laizy.marcelites@escola.pr.gov.br>
2026-09-24 09:35:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!?*@%';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
         //tamanhoSenha = tamanhoSenha-1;
